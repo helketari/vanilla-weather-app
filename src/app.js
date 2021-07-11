0d70835238130ad09320f9255f1df4733b6a3609
helketari <helketari@comcast.net>
2021-07-11 17:23:50 -0400

    Created API for forecast in JS

--- src/app.js
+++ src/app.js
@@ -43,15 +43,21 @@ function formatDate() {
 
 formatDate();
 
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
   let forecastHTML = `<div class="row">`;
+
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
+
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
-      `<div class="col-2">
+      `
+      
+      <div class="col-2">
       <div class="forecast-day">${day}</div>
       <img src="http://openweathermap.org/img/wn/50d@2x.png"
       alt=""
@@ -67,6 +73,13 @@ function showForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "14b04c33525e63089effa5297a33ce92";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function showTemperature(response) {
